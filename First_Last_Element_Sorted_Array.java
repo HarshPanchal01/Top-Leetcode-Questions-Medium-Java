77fee133b5089342c930e00e8c8a089fdf536bc4
Harsh <harshpanchal30x@gmail.com>
2024-01-08 20:49:27 -0500

    in progress

---
+++ First_Last_Element_Sorted_Array.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class First_Last_Element_Sorted_Array {
+    public static int[] searchRange(int[] nums, int target) {
+
+    }
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8, target2 = 6, target3 = 0;
+        int[] nums2 = {};
+
+        Arrays.toString(searchRange(nums, target));
+        Arrays.toString(searchRange(nums, target2));
+        Arrays.toString(searchRange(nums2, target3));
+    }
+}
