951c3810341ce23e64eb29663b7959c1b01da63a
HarshPanchal01 <harshpanchal30x@gmail.com>
2024-05-10 19:14:20 -0400

    in progress

---
+++ Spiral_Matrix.java
@@ -0,0 +1,12 @@
+public class Spiral_Matrix {
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        int[][] matrix2 = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+
+        System.out.println(spiralOrder(matrix));
+        System.out.println(spiralOrder(matrix2));
+    }
+}
