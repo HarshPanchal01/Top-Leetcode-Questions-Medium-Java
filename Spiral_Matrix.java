767a026a18784888d2b340d85c09d5ee56ac5234
HarshPanchal01 <harshpanchal30x@gmail.com>
2024-05-12 19:52:52 -0400

    solved

--- Spiral_Matrix.java
+++ Spiral_Matrix.java
@@ -1,6 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Spiral_Matrix {
     public static List<Integer> spiralOrder(int[][] matrix) {
-        
+        List<Integer> result = new ArrayList<>();
+        int left = 0, right = matrix[0].length - 1;
+        int top = 0, bottom = matrix.length - 1;
+
+        while (left <= right && top <= bottom) {
+            // get every element in top row
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+            // get every element in right column
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+            // edge-case
+            if (left > right || top > bottom) {
+                break;
+            }
+            // get every element in the bottom row
+            for (int i = right; i >= left; i--) {
+                result.add(matrix[bottom][i]);
+            }
+            bottom--;
+            // get every element in the left column
+            for (int i = bottom; i >= top; i--) {
+                result.add(matrix[i][left]);
+            }
+            left++;
+        }
+        return result;
     }
     public static void main(String[] args) {
         int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
